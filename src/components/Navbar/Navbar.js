5091b1d863dc9c2e715c0c7d8ad9dd6d13a241be
DalineSeritsane <dalinelerato@gmail.com>
2025-05-21 11:49:28 +0200

    navbar menu

--- src/components/Navbar/Navbar.js
+++ src/components/Navbar/Navbar.js
@@ -4,6 +4,7 @@ import logo from '../Assets/logo.jpg';
 import cartIcon from '../Assets/cart.jpg';
 import { Link } from 'react-router-dom';
 import { ShopContext } from '../../Context/ShopContext';
+import nav_dropdown from '../Assets/dropdown.jpg';
 
 const Navbar = () => {
 
@@ -12,14 +13,22 @@ const Navbar = () => {
   const {getTotalCartItems} = useContext(ShopContext);
   const menuRef = useRef();
 
+  const dropdown_toggle = (e) => {
+    menuRef.current.classList.toggle('nav-menu-visible');
+    e.target.classList.toggle('open');
+
+
+  }
+
   return (
     <nav className="navbar">
       <div className="navbar-logo">
         <img src={logo} alt="Sneaker Fit Logo" />
         <span className="brand-name">SNEAKER FiT</span>
       </div>
+      <img className="nav-dropdown" onClick={dropdown_toggle} src={nav_dropdown} alt=''/>
 
-      <ul className="navbar-menu">
+      <ul ref={menuRef} className="navbar-menu">
         <li onClick={() => { setMenu("home") }}>
           <Link to='/'>Home</Link>
           {menu === "home" ? <hr /> : <></>}
